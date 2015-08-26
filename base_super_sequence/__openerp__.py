1096f224137f94074a9adbfd64e451a3b9e3587c
Graeme Gellatly <gdgellatly@gmail.com>
2015-08-27 11:46:44 +1200

    [WIP] base_super sequence

---
+++ base_super_sequence/__openerp__.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+# OpenERP / Odoo, Open Source Management Solution - module extension
+#    Copyright (C) 2014- O4SB (<http://openforsmallbusiness.co.nz>).
+#    Author Graeme Gellatly <g@o4sb.com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    'name': 'Super Sequence',
+    'version': '0.1',
+    'category': 'Technical',
+    'author': 'Graeme Gellatly - O4SB',
+    'website': 'http://www.openforsmallbusiness.co.nz',
+    'depends': [],
+    "description": """
+    This module introduces a super sequence where the same number is used for
+    all related documents to a model for
+    Sales
+    Manufacturing
+    Accounting
+    Purchasing
+    Warehouse
+    """,
+    "data": [],
+    'demo_xml': [],
+    'installable': True,
+    'active': False,
+}
+
---
+++ base_super_sequence/sequence.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+# OpenERP / Odoo, Open Source Management Solution - module extension
+#    Copyright (C) 2014- O4SB (<http://openforsmallbusiness.co.nz>).
+#    Author Graeme Gellatly <g@o4sb.com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from openerp.osv import orm, fields
+
+
+class SuperSequence(orm.Model):
+    """Super Sequence"""
+    _name = 'super.sequence'
+    _description = __doc__
+
+    columns = {'%s_increment' % f: fields.integer(string='%s' % f) for f in
+               ('sale, out, in, mrp, purchase, '
+                'out_invoice, out_refund, in_invoice, in_refund')}
+
+    _columns = dict({'linked_doc_ids': fields.one2many(
+        'linked.document', 'seq_id', string='Linked Documents',
+        auto_join=True)}, columns)
+
+    _defaults = {'%s_increment' % f: 0 for f in
+                 ('sale, out, in, mrp, purchase, '
+                  'out_invoice, out_refund, in_invoice, in_refund')}
+
+
+class LinkedDocument(orm.Model):
+    """Linked Document"""
+    _name = 'linked.document'
+
+    _columns =
